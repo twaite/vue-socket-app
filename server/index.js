4fd6b2916ed0b37e184e7fd5c5ce5d36de05c24e
Tim Waite <tim@dialexa.com>
2021-02-09 22:40:44 -0600

    Add server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log(
+    "a user connected, info: ",
+    socket,
+    socket.request.connection._peername
+  );
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
